551d9fdfa423b221aaca1a02c7fbcc4ec6494142
jun-kondo <ranmaru.oo.q@gmail.com>
2024-07-29 23:50:06 +0900

    Use SWR Library

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import useSWR from "swr";
 import "./App.css";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
+  const fetcher = (url) => {
+    return fetch(url, { headers })
+      .then((res) => res.json())
+      .then((json) => json.description);
+  };
+  const { data: status, error, isLoading } = useSWR(url, fetcher);
 
-  const [status, setStatus] = useState("");
-
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
+  if (error) return <div>Failed to load.</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return <>{status && <p>Status : {status}</p>}</>;
 }
